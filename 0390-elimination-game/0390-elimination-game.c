b955f8519b6f81c7386278c3d4fc33a9f5980f4f
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-07-06 16:25:06 +0530

    Time: 0 ms (100%), Space: 5.6 MB (56%) - LeetHub

---
+++ 0390-elimination-game/0390-elimination-game.c
@@ -0,0 +1,17 @@
+int lastRemaining(int n) {
+    if(n==1)
+    return 1; 
+    return 1+fun(n,1) ;
+}
+
+   int fun(int n , int d){
+         if(n==2&&d==-1)
+        return  0 ;
+       if(n<=3)
+       return 1;
+       
+       
+        if((n%2==0)&&(d==-1))
+        return 2*fun(n/2 , -d);
+        return 1 + 2*fun(n/2 , -d) ;
+    }
