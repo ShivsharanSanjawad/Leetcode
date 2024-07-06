c0985846cc50c76b493d57381a26fbb8ed24fc8e
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-07-06 16:13:25 +0530

    Time: 2 ms (48.26%), Space: 41.5 MB (55.67%) - LeetHub

---
+++ 0390-elimination-game/0390-elimination-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lastRemaining(int n) {
+        if(n==1)
+        return 1;
+        return 1+ fun(n,1);
+    }
+
+    public int fun(int n , int d){
+        if(n==3)
+        return 1;
+        if(n==2&&d==1)
+        return 1;
+        if(n==2&&d==-1)
+        return  0 ;
+       
+        if((n%2==0)&&(d==-1))
+        return 2*fun(n/2 , -d);
+        return 1 + 2*fun(n/2 , -d) ;
+    }
+}
