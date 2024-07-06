32767eaf114b19e540e323228d57ccfb2f3eab4b
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-07-06 22:09:30 +0530

    Time: 10 ms (36.6%), Space: 59 MB (39.72%) - LeetHub

--- 2722-prime-in-diagonal/2722-prime-in-diagonal.java
+++ 2722-prime-in-diagonal/2722-prime-in-diagonal.java
@@ -1,11 +1,12 @@
 class Solution {
       public boolean isPrime(int n){
-        if(n==1)
-        return false ;
+        
         for(int i =2 ; i<=Math.sqrt(n); i++){
             if(n%i==0)
             return false; 
         }
+        if(n==1)
+        return false ;
         return true ;
     }
     public int diagonalPrime(int[][] nums) {
