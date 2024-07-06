9948c9a7dbd3268d98ca0432c335221ffe769444
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-07-06 16:20:01 +0530

    Time: 2 ms (48.26%), Space: 41.6 MB (38.88%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.java
+++ 0390-elimination-game/0390-elimination-game.java
@@ -6,12 +6,11 @@ class Solution {
     }
 
     public int fun(int n , int d){
-        if(n==3)
-        return 1;
-        if(n==2&&d==1)
-        return 1;
-        if(n==2&&d==-1)
+         if(n==2&&d==-1)
         return  0 ;
+       if(n<=3)
+       return 1;
+       
        
         if((n%2==0)&&(d==-1))
         return 2*fun(n/2 , -d);
