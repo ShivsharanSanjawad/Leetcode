5c6c41da78b2eade88debdae815b87ed3ca22c9d
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-07-09 14:32:12 +0530

    Time: 1 ms (66.54%), Space: 40.5 MB (65.52%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isHappy(int n) {
+        
+        int a = 0 ; 
+        while(n!=1)
+        {    int temp = n ; 
+            int sum = 0 ; 
+            for(int i  = 0  ; temp>0 ; i++)
+            {
+                sum = sum + (temp%10)*(temp%10) ; 
+                temp = temp /10 ; 
+            }
+            n = sum ; 
+            a++ ;
+            if(a==100)
+            return false ; 
+        }
+        return true ; 
+    
+    }
+}
