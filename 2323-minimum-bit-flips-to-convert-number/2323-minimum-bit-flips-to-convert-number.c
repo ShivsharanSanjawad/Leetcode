2f5c835a4c6982cfd1e1c5e6314921174d9a8127
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-07-09 23:24:25 +0530

    Time: 3 ms (34.21%), Space: 5.6 MB (31.05%) - LeetHub

---
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.c
@@ -0,0 +1,13 @@
+int minBitFlips(int start, int goal) {
+     int ans = start^goal ;
+        int count = 0 ; 
+        while(ans>0){
+           if(ans%2==1) {
+                count ++ ;
+            }
+            ans = ans/2 ;
+           
+        }
+        return count ;
+        
+}
