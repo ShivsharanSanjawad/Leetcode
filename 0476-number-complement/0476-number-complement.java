aa4b50ccc5e0d05c1277486a39db191a338520e5
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-07-27 15:20:42 +0530

    Time: 0 ms (100%), Space: 40.7 MB (16.28%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findComplement(int n) {
+         if(n==0)
+        return 1 ; 
+        int x = n ; 
+        int y = 1;
+        while(x>0){
+            n = n^y ; 
+            y = y<<1 ; 
+           x = x>>1 ; 
+        }
+        return n ;
+    }
+}
