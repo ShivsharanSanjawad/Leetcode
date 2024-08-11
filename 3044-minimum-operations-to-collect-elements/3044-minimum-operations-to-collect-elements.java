6a86f37bd7e2c191c4c252c49006e29dbabbe385
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-08-11 21:37:39 +0530

    Time: 1 ms (99.16%), Space: 41.8 MB (81.59%) - LeetHub

--- 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.java
+++ 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.java
@@ -1,24 +1,25 @@
+import java.util.List;
+
 class Solution {
     public int minOperations(List<Integer> nums, int k) {
-        int x = 0 ; 
-        int z = nums.size() - 1 ; 
-        int count = 0 ;
-        int a[] = new int[k + 1] ;
-       // System.out.println(Arrays.toString(a)) ;
+        int x = 0; 
+        int z = nums.size() - 1; 
+        int count = 0;
+        boolean[] a = new boolean[k + 1];
+
         while (z >= 0) {
             if (x == k) {
-                return count ;
+                return count;
             }
-            try {
-                if (a[nums.get(z--)] == 0) {
-                    a[nums.get(z+1)] = 1 ;
-                    x++ ;
-               }
-            } catch (Exception e) {
-              
+
+            int num = nums.get(z--);
+            if (num <= k && !a[num]) {
+                a[num] = true;
+                x++;
             }
-            count++ ;
+
+            count++;
         }
-        return count ;
+        return count;
     }
 }
