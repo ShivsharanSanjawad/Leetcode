3cf9e8049cf4873e1afea0eac4b670afab731c14
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-08-12 22:35:33 +0530

    Time: 68 ms (81.78%), Space: 19.6 MB (24.75%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -2,10 +2,10 @@ void moveZeroes(int* nums, int n) {
     int x = 0 ; 
     for(int i = 0 ; i<n ;i++){
         if(nums[i]!=0){
-            nums[x++] = nums[i] ;
-            if(x-1!=i){
-                nums[i]= 0 ;
-            }
+            int temp = nums[i] ; 
+            nums[i] = nums[x] ; 
+            nums[x] =temp;
+           x++ ;
         }
     }
 }
