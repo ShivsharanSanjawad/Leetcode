7b1d1aa4f5b894d65dd260043f194792f5f3b469
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-08-18 15:00:02 +0530

    Time: 0 ms (100%), Space: 40.8 MB (92.34%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -1,44 +1,50 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if(head==null)
-        return null ;
-        ListNode prev = null ; 
-        ListNode curr  = head ;
-        ListNode next = head.next ;
-       for(int i = 0 ; i<left-1 ; i++){
-             prev = curr ; 
-             curr = next ; 
-             next = next.next ;
-       }
-       ListNode last = prev ; 
-       ListNode end = curr ;
-       for(int i = 0 ;i<right-left+1 ; i++){
-            curr.next = prev ; 
-            prev = curr ; 
-            curr = next ; 
-            if(next!=null)
-            next = next.next ;
-       }
-       if(last!=null){
-        last.next = prev ;
-       }else{
-        head = prev ;
-       }
-       
-       end.next = curr ; 
-       return head ;
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode x = head;
+        ListNode prev = null;
+        ListNode next = head.next;
+        for (int i = 0; i < (left - 1); i++) {
+            prev = x;
+            x = next;
+            next = next.next;
+        }
+        // x = prev ;
+        ListNode y = x;
+        x= prev ; 
+        ListNode past = prev;
+        ListNode present = y;
+        ListNode future = next;
+        int count = 1 + (right - left);
+        while (count != 0) {
+            present.next = past;
+            past = present;
+            present = future;
+            if (future != null) {
+                future = future.next;
+            }
+            count--;
+        }
 
+        if (x != null) {
+            x.next = past;
+        } else {
+            head = past;
+        }
+        if (y != null)
+            y.next = present;
+        return head;
     }
-    
 }
