fce1d309a71a8c96167fd71745b1e4507f8e75db
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-09-03 17:01:32 +0530

    Time: 4 ms (66.26%), Space: 55.6 MB (28.7%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
@@ -1,19 +1,15 @@
 class Solution {
-    int a[] ;
-     int x = 0 ; 
-    public int get(){
-        return a[x++] ; 
-    }
+    
     public int[][] construct2DArray(int[] original, int m, int n) {
         if(m*n!=original.length){
             return new int [][]{} ; 
         }
-        this.a = original ; 
+        int x = 0 ; 
         int ans [][] = new int[m][n] ; 
         for(int i = 0 ; i<m ; i++){
             for(int j = 0 ; j<n ; j++){
               
-                ans[i][j] = get() ; 
+                ans[i][j] = original[x++] ; 
             }
         }
         return ans ; 
