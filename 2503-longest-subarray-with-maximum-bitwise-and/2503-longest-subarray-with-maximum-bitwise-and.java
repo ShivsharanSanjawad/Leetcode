b4cdf71d372d992db24ddf7dbe42484dff66f6e7
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-09-14 15:53:29 +0530

    Time: 4 ms (22.47%), Space: 59.4 MB (43.82%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.java
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.java
@@ -1,26 +1,23 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int maxVal = nums[0];    // Track the maximum value
-        int maxLength = 0;       // Length of the longest subarray
-        int currentLength = 0;   // Current length of the subarray
+        int prev = nums[0]; 
+        int max = 0; 
+        int prevmax = prev; 
+        int length = 0; 
 
-        // Single loop for both max calculation and subarray length tracking
         for (int i = 0; i < nums.length; i++) {
-            // Update max value if current number is larger
-            if (nums[i] > maxVal) {
-                maxVal = nums[i];
-                currentLength = 1; // Reset current subarray length
-                maxLength = 1;     // Reset max length
-            } else if (nums[i] == maxVal) {
-                // Extend the current subarray if it's equal to max value
-                currentLength++;
-                maxLength = Math.max(maxLength, currentLength);
+            if (nums[i] > prev) {
+                prev = nums[i];
+                length = 1;
+                max = 1;
+            } else if (nums[i] == prev) {
+                length++;
+                max = Math.max(max, length);
             } else {
-                // Reset the current subarray length if we find a smaller value
-                currentLength = 0;
+                length = 0;
             }
         }
 
-        return maxLength;
+        return max;
     }
 }
