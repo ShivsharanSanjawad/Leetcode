832ca2597fcef09db2e2819dd4665656f6234c76
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-09-17 10:09:46 +0530

    Time: 2 ms (100%), Space: 42.1 MB (54.6%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
@@ -1,50 +1,25 @@
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
       HashMap<String ,Integer> h  = new HashMap<>() ; 
-      String s = "" ;
-      for(int i = 0 ;i<s1.length() ; i++){
-        if(s1.charAt(i)!=' '){
-            s = s + s1.charAt(i) ; 
-        }else if(!s.equals("")){
-           if(!h.containsKey(s))
-               { h.put(s,1) ; 
-                
-               }else h.put(s,h.get(s)+1) ;
-            s = "" ; 
-        }else{
-            s = "" ;
-        }
+     String x1[] = s1.split(" ") ; 
+     String x2 [] = s2.split(" ") ;
+      for(int i = 0 ;i<x1.length ; i++){
+        if(h.containsKey(x1[i])){
+            h.put(x1[i],h.get(x1[i])+1) ;
+        }else 
+        h.put(x1[i],1) ;
       }
-      if(s!=""){
-         if(!h.containsKey(s))
-        h.put(s,1) ; 
-        else h.put(s,h.get(s)+1) ;
+ for(int i = 0 ;i<x2.length ; i++){
+        if(h.containsKey(x2[i])){
+            h.put(x2[i],h.get(x2[i])+1) ;
+        }else 
+        h.put(x2[i],1) ;
       }
-      s = "" ;
       ArrayList<String> ans = new ArrayList<>() ; 
-       for(int i = 0 ;i<s2.length() ; i++){
-        if(s2.charAt(i)!=' '){
-            s = s + s2.charAt(i) ; 
-        }else if(!s.equals("")){
-            if(!h.containsKey(s))
-               { h.put(s,1) ; 
-                
-               }else h.put(s,h.get(s)+1) ;
-            s = "" ; 
-        }else s = "" ; 
+      for(String x : h.keySet()){
+        if(h.get(x)==1)
+        ans.add(x) ;
       }
-      if(s!=""){
-         if(!h.containsKey(s))
-               { h.put(s,1) ; 
-                
-               }else h.put(s,h.get(s)+1) ;
-      }
-      for(String key :h.keySet()){
-        if(h.get(key)==1)
-        
-            ans.add(key) ; 
-      }
-    
       String a[] = new String[ans.size() ] ; 
       for(int i = 0 ;i<a.length; i++){
         a[i] = ans.get(i)  ; 
