f207e80f9b191d16f863ca69f0ae16e9d7d0fef3
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-09-23 14:20:54 +0530

    Time: 2 ms (91.77%), Space: 52.6 MB (5.37%) - LeetHub

--- 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.java
+++ 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.java
@@ -1,20 +1,10 @@
 class Solution {
     public int[][] sortTheStudents(int[][] scores, int k) {
-        
-        for(int i = 0 ; i<scores.length-1 ; i++){
-            boolean flag = true ;
-            for(int j = 0 ; j<scores.length-i-1; j++){
-                if(scores[j][k]<scores[j+1][k]){
-                    int temp[] = scores[j] ; 
-                    scores[j] = scores[j+1] ; 
-                    scores[j+1] = temp ; 
-                    flag = false ;
-                }
-            }
-            if(flag){
-                break ;
-            }
+       Arrays.sort(scores , new Comparator<int[]>(){
+        public int compare(int a[] , int b[]){
+            return b[k]-a[k];
         }
-        return scores ;
+       }) ;
+       return scores ;
     }
 }
