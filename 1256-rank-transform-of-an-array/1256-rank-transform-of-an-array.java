a2a290f65e22eed3ad4a169def3e302f0dd5b0f2
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-10-02 08:08:41 +0530

    Time: 26 ms (69.92%), Space: 62.1 MB (51.49%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 
 public class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int[] temp = Arrays.copyOf(arr, arr.length);  
+        int[] temp =arr.clone();  
         Arrays.sort(temp);  
         
         HashMap<Integer, Integer> rankMap = new HashMap<>();  
