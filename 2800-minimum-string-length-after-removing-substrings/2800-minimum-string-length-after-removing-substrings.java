7cf4debd38de759b6ede2761913c67a709fa02ad
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-10-07 23:56:27 +0530

    Time: 5 ms (65.3%), Space: 43.1 MB (71.14%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -1,21 +1,22 @@
 class Solution {
-    public boolean isok(char s1, char s2){
-        return (s1=='A'&&s2=='B')||(s1=='C'&&s2=='D') ;
-    }
     public int minLength(String s) {
        Stack<Character> st = new Stack<>() ; 
        st.push(s.charAt(0)) ; 
        int i = 1 ; 
-       while(i<s.length()){
+       int x = s.length() ;
+       while(i<x){
         boolean flag = true ; 
-        if(st.size()>0)
-            if(isok(st.peek() ,s.charAt(i))){
+         char s2 = s.charAt(i) ;
+        if(st.size()>0){
+           char s1 = st.peek() ; 
+            if((s1=='A'&&s2=='B')||(s1=='C'&&s2=='D')){
                // System.out.println(st.peek()+" in") ;
                 st.pop() ; 
                 flag = false ;
             }
+        }
             if(flag) 
-            st.push(s.charAt(i)) ;
+            st.push(s2) ;
 //System.out.println(st.peek()+" out") ;
             i++ ; 
        }
