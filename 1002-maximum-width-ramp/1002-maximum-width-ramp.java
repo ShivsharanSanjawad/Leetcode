1d12508c7bf376501ed890ac29d43e9efb2a46b3
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-10-10 19:55:47 +0530

    Time: 4 ms (84.83%), Space: 55.2 MB (24.13%) - LeetHub

---
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        int prefix [] = new int[nums.length] ; 
+        prefix [nums.length-1] = nums[nums.length-1] ;
+        for(int i  = nums.length -2 ; i>=0 ;i--){
+            prefix[i] = Math.max(prefix[i+1],nums[i]) ;
+        }
+        int max = Integer.MIN_VALUE ; 
+        int i = 0 ; 
+        int j = 0 ; 
+        while(j<nums.length){
+        while(j>i){
+            if(prefix[j]<nums[i]){
+                i++ ; 
+            }else break ;
+        }
+        if((j-i)>max){
+            max = j-i ;
+        }
+        j++;
+        }
+        return max ;
+    }
+}
