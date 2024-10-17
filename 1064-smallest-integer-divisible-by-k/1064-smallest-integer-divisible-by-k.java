0b8e472934546d3506fcfc197baa87199c6dd921
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-10-17 22:24:16 +0530

    Time: 15 ms (5.3%), Space: 46.7 MB (10.61%) - LeetHub

--- 1064-smallest-integer-divisible-by-k/1064-smallest-integer-divisible-by-k.java
+++ 1064-smallest-integer-divisible-by-k/1064-smallest-integer-divisible-by-k.java
@@ -1,7 +1,5 @@
 class Solution {
     public int smallestRepunitDivByK(int k) {
-        if(k%2==0 || k%5==0)
-            return -1;
         HashSet<Integer> h = new HashSet<>() ;
         int rem10 = 10%k ; 
         int x = 1 ; 
