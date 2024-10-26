d1c16790a9a87ff28568a5b291f762524d747fe7
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-10-26 11:08:14 +0530

    Time: 0 ms (100%), Space: 40.7 MB (19.39%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -3,9 +3,9 @@ class Solution {
         int z = x^y ;
         int count = 0  ; 
         while(z>0){
-            if(z%2==1)
+            if((z&1)==1)
             count++ ; 
-            z = z/2 ; 
+            z = z>>1 ; 
         }
         return count ;
     }
