dbf0bc3b4ca1c050b5b193fa63959442e2039eda
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-10-30 15:30:46 +0530

    Time: 86 ms (11.43%), Space: 44.5 MB (30.95%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.java
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.java
@@ -1,54 +1,51 @@
 import java.util.Arrays;
 
 class Solution {
-    int[] left, right;
-    
-    // Helper function for the increasing subsequence from the start up to index i
-    public int getIncreasing(int[] nums, int i) {
-        if (left[i] != -1) return left[i];
-        
+    int[] dp;
+    int []dp2 ; 
+    public int get(int[] nums, int i) {
+        if (dp[i] != -1) {
+            return dp[i];
+        }
         int max = 1;
         for (int j = 0; j < i; j++) {
-            if (nums[j] < nums[i]) {
-                max = Math.max(max, getIncreasing(nums, j) + 1);
+            if (nums[i] > nums[j]) {
+                max = Math.max(max, get(nums, j) + 1);
             }
         }
-        left[i] = max;
-        return left[i];
+        dp[i] = max;
+        return dp[i];
     }
 
-    // Helper function for the decreasing subsequence from the end down to index i
-    public int getDecreasing(int[] nums, int i) {
-        if (right[i] != -1) return right[i];
-        
+    public int get2(int[] nums, int i) {
+        if (dp2[i] != -1) {
+            return dp2[i];
+        }
         int max = 1;
-        for (int j = i + 1; j < nums.length; j++) {
-            if (nums[j] < nums[i]) {
-                max = Math.max(max, getDecreasing(nums, j) + 1);
+        for (int j =i; j <nums.length; j++) {
+            if (nums[i] > nums[j]) {
+                max = Math.max(max, get2(nums, j) + 1);
             }
         }
-        right[i] = max;
-        return right[i];
+        dp2[i] = max;
+        return dp2[i];
     }
 
     public int minimumMountainRemovals(int[] nums) {
-        int n = nums.length;
-        left = new int[n];
-        right = new int[n];
-        Arrays.fill(left, -1);
-        Arrays.fill(right, -1);
-        
-        int maxMountain = 0;
-        
-        for (int i = 1; i < n - 1; i++) {
-            int leftLIS = getIncreasing(nums, i);
-            int rightLIS = getDecreasing(nums, i);
+        int max = 0;
+        dp = new int[nums.length];
+        dp2 = new int[nums.length];
+        Arrays.fill(dp,-1);
+        Arrays.fill(dp2,-1);
 
-            if (leftLIS > 1 && rightLIS > 1) {
-                maxMountain = Math.max(maxMountain, leftLIS + rightLIS - 1);
+        for (int i = 1; i < nums.length - 1; i++) {
+            int left = get(nums,i); 
+            int right = get2(nums, i); 
+
+            if (left > 1 && right > 1) { 
+                max= Math.max(max, left + right - 1);
             }
         }
-        
-        return n - maxMountain;
+        return nums.length - max;
     }
 }
