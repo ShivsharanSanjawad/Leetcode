1919f88a9bd167737e866f0ef85b176573535c1f
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-11-02 08:21:49 +0530

    Time: 1 ms (96.81%), Space: 41.9 MB (54.87%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.java
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean isCircularSentence(String s) {
-        if(s.charAt(0)!=s.charAt(s.length()-1)){
+        int n = s.length() -1 ;
+        if(s.charAt(0)!=s.charAt(n)){
             return false ;
         }
         char prev = 0 ; 
-        for(int i = 0 ; i<s.length() -1 ; i++){
+   
+        for(int i = 0 ; i<n ; i++){
             if(s.charAt(i)==' '){
                 if(s.charAt(i+1)!=prev){
                     return false ;
