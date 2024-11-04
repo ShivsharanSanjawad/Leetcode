8c4c7b99b6ad2af8dcb8ef51493bbcb25775dd2c
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-11-04 17:50:22 +0530

    Time: 7 ms (50%), Space: 14.8 MB (62.5%) - LeetHub

---
+++ 3451-string-compression-iii/3451-string-compression-iii.c
@@ -0,0 +1,23 @@
+char* compressedString(char* word) {
+    char * s = malloc(strlen(word)*sizeof(char)*3) ;
+    int x = 0 ;  
+    for(int i = 0 ; word[i]!=0;){
+        char ch = word[i] ;
+        int count = 0 ;
+        while(word[i]!=0&&ch==word[i]){
+            i++ ; 
+            count++ ; 
+        }
+        while(count>9){
+            s[x++] = 9+'0';
+            s[x++] = ch ; 
+            count -=9 ;
+        }
+        if(count!=0){
+            s[x++] = count+'0';
+            s[x++] = ch ;
+        }
+    }
+    s[x] = 0 ; 
+    return s ;
+}
