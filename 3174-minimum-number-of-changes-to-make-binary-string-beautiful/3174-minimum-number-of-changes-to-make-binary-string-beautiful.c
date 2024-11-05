5cb1ca1d448b74b1047b20fc0171c167c35f8170
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-11-05 21:50:56 +0530

    Time: 0 ms (100%), Space: 11.4 MB (100%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.c
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.c
@@ -1,19 +1,4 @@
 int minChanges(char* s) {
-    int count = 0 ; 
-    int flag  = 0 ; 
-    int n = strlen(s) ; 
-    for(int i = 0 ; i<n ; ){
-        int ch = s[i] ; 
-        int temp =  flag;
-        while(ch==s[i]){
-            i++ ;
-            temp++  ;
-        }
-        if((temp&1)){
-            s[i-1] = s[i] ; 
-            flag = 1 ; 
-            count ++ ;
-        }else flag = 0 ;
-    }
-    return count ;
+    if(s[0]==0) return 0 ;
+  return (s[0]!=s[1]) +  minChanges(s+2) ;
 }
