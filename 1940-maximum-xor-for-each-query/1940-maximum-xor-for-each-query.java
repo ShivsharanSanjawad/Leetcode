f46e79ff856ee6a89b37a8e767850a83641cb47f
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-11-08 20:31:19 +0530

    Time: 3 ms (78.95%), Space: 58.7 MB (73.09%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.java
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.java
@@ -1,15 +1,17 @@
 class Solution {
     public int[] getMaximumXor(int[] nums, int m) {
         int x = (1<<m) - 1 ;
-        int [] prefix = new int[nums.length] ; 
+        // int [] prefix = new int[nums.length] ; 
+        int [] ans = new int[nums.length];
         int xor = 0 ; 
         for(int i = 0 ; i<nums.length;i++){
             xor = xor^nums[i] ; 
-            prefix[nums.length-1-i] = xor ;
+            // prefix[nums.length-1-i] = xor ;
+            ans[nums.length-1-i] = xor^x ;
         }
-        for(int i = 0 ;i<nums.length;i++){
-            nums[i] = prefix[i]^x ;
-        }
-        return nums ;
+        // for(int i = 0 ;i<nums.length;i++){
+        //     nums[i] = prefix[i]^x ;
+        // }
+        return ans ;
     }
 }
