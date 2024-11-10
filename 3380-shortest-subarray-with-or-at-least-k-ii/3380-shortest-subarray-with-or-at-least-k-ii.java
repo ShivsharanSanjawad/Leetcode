ea2d6706e934fd98e09b8da727faea577de334eb
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-11-10 18:56:59 +0530

    Time: 40 ms (91.11%), Space: 69.7 MB (68.75%) - LeetHub

--- 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.java
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.java
@@ -1,36 +1,44 @@
 class Solution {
     int[] a;
-    int[] b;
+    // int[] b;
+    int x ;
 
     public void add(int n) {
-        for (int i = 0; n > 0; i++) {
+        for (int i = 0;i< a.length; i++) {
             a[i] += (n & 1);
             n >>= 1;
         }
     }
-
     public void remove(int n) {
-        for (int i = 0; n > 0; i++) {
+        for (int i = 0; i<a.length; i++) {
             a[i] -= (n & 1);
             n >>= 1;
         }
     }
 
     public void set(int k) {
-        int i = 0;
-        while (k > 0) {
-            b[i++] = (k & 1);
-            k >>= 1;
-        }
+        // int i = 0;
+        // while (k > 0) {
+        //     b[i++] = (k & 1);
+        //     k >>= 1;
+        // }
+        this.x = k ;
     }
 
     public boolean isok() {
-        for (int i = 0; i < a.length; i++) {
-            if (b[i] > a[i]) {
-                return false;
+        // for (int i = 0; i < a.length; i++) {
+        //     if (b[i] > a[i]) {
+        //         return false;
+        //     }
+        // }
+        // return true;
+        int z = 0 ;
+        for(int i =0 ; i<a.length ; i++){
+            if(a[i]>0){
+                z = z|(1<<i);
             }
         }
-        return true;
+        return z>=x ;
     }
 
     public int minimumSubarrayLength(int[] nums, int k) {
@@ -38,7 +46,8 @@ class Solution {
             return 1;
         }
         a = new int[32];
-        b = new int[32];
+        // b = new int[32];
+        // set(k);
         set(k);
 
         int min = Integer.MAX_VALUE;
@@ -46,12 +55,12 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             add(nums[i]);
-            while (j < nums.length && isok()) {
+            while (j <=i&& isok()) {
                 min = Math.min(min, i - j + 1);
                 remove(nums[j++]);
             }
+           
         }
-
         return (min == Integer.MAX_VALUE) ? -1 : min;
     }
 }
