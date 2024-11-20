6b99b9bd6774f0f751d6c949621e6d1c20ae66de
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-11-21 00:23:53 +0530

    Time: 8 ms (21.14%), Space: 44.5 MB (63.61%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.c
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.c
@@ -1,43 +1,53 @@
-struct ListNode* reverse(struct ListNode* head) {
-    struct ListNode* prev = NULL;
-    struct ListNode* curr = head;
-    struct ListNode* next = NULL;
-    while (curr != NULL) {
-        next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+#define Node struct ListNode*
+
+
+Node reverse(Node head){
+    if(head==NULL){
+        return NULL ;
+    }
+    Node prev = NULL ; 
+    Node curr = head;
+    Node next = head->next  ;
+    while(curr!=NULL){
+
+        curr->next = prev ; 
+        prev = curr ; 
+        curr = next ; 
+        if(next!=NULL){
+            next= next->next;
+        }
     }
     return prev;
 }
 
-struct ListNode* mid(struct ListNode* head) {
-    struct ListNode* slow = head;
-    struct ListNode* fast = head;
-    while (fast != NULL && fast->next != NULL) {
-        slow = slow->next;
-        fast = fast->next->next;
+Node mid(Node head){
+    Node slow  = head ;
+    Node fast = head ;
+    while(fast!=NULL&&fast->next!=NULL){
+        slow = slow->next ; 
+        fast = fast->next->next ;
     }
-    return slow;
+    return slow ;
 }
 
 bool isPalindrome(struct ListNode* head) {
-    if (head == NULL || head->next == NULL) {
+     Node temp = mid(head);
+    Node   reversed = reverse(temp);
+        temp = reversed ; 
+        while(temp!=NULL){
+            if(temp->val!=head->val){
+                return false;
+            }
+            temp = temp->next;
+            head = head->next;
+        } 
         return true;
-    }
 
-    struct ListNode* middle = mid(head);
-    struct ListNode* reversed = reverse(middle);
-
-    struct ListNode* p1 = head;
-    struct ListNode* p2 = reversed;
-
-    while (p2 != NULL) {
-        if (p1->val != p2->val) {
-            return false;
-        }
-        p1 = p1->next;
-        p2 = p2->next;
-    }
-    return true;
 }
