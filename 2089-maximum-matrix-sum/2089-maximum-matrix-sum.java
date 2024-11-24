f224c9370c8e08fe8910b8344f378d2337050101
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-11-24 22:03:13 +0530

    Time: 7 ms (29.87%), Space: 54.4 MB (99.08%) - LeetHub

--- 2089-maximum-matrix-sum/2089-maximum-matrix-sum.java
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.java
@@ -7,39 +7,21 @@ class Solution {
         //     }
         //     System.out.println();
         // }
-        ArrayList<Integer> ans = new ArrayList<>() ; 
-        int a[] = new int[matrix.length] ;
-        Arrays.fill(a,0);
-        for(int i = 0 ; i<matrix.length ; i++){
-            for(int j = 0 ; j<matrix[i].length ; j++){
+        int count = 0 ; 
+        int min = Integer.MAX_VALUE ;
+        for(int i  = 0 ; i<matrix.length ;i++){
+            for(int  j = 0 ; j<matrix[0].length ;j++){
+                sum +=Math.abs(matrix[i][j]);
                 if(matrix[i][j]<0){
-                    a[i]++ ;
-                    matrix[i][j] = -matrix[i][j] ;
+                    count++ ;
+                }
+                if(Math.abs(matrix[i][j])<min){
+                    min = Math.abs(matrix[i][j]) ;
                 }
             }
         }
-        int count = 0 ;
-        for(int i = 0 ; i<matrix.length;i++){
-            Arrays.sort(matrix[i]);
-            a[i] = a[i]%2 ; 
-            if(a[i]==1){
-                count++ ;
-            }
-        }
-        for(int i = 0 ; i<matrix.length ; i++){
-                ans.add(matrix[i][0]);
-            for(int j = 1 ; j<matrix[i].length ; j++){
-               sum +=matrix[i][j] ;
-            }
-        }
-        Collections.sort(ans) ; 
-        if(count%2==1){
-            sum-=ans.get(0);
-        }else{
-            sum +=ans.get(0);
-        }
-        for(int i =1 ; i<ans.size();i++){
-            sum+=ans.get(i);
+        if(count%2!=0){
+            sum -=2*min ;
         }
         return sum;
     }
