5bb2551c0aeeaa3d2cc556db29907df7c2f4a35b
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-11-28 01:07:29 +0530

    Time: 2145 ms (5.71%), Space: 46 MB (5.74%) - LeetHub

--- 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.java
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.java
@@ -1,32 +1,35 @@
 class Solution {
+    int [] dp  ;
     public int bfs(int a[][],int k){ 
-       Queue<Integer> q = new LinkedList<>() ; 
-       q.add(k) ; 
-       int distance[] = new int [a.length] ;
-       distance[0] = 0 ; 
-       boolean visited [] = new boolean[a.length];
-       visited[0] = true ;
-       while(!q.isEmpty()){
-        int j = q.remove() ;
-            for(int i = 0 ; i<a.length;i++){
-                if(a[j][i]==1&&!visited[i]){
-                    q.add(i);
-                    visited[i] = true ;
-                    distance[i] = distance[j] +1 ; 
-                }
+        if(k==a.length-1){
+            return 0 ; 
+        }
+        if(dp[k]!=-1){
+            return dp[k] ; 
+        }
+        int min = Integer.MAX_VALUE ;
+        for(int i = 0 ; i<a.length ;i++){
+            if(a[k][i]==1){
+                int y  = bfs(a,i) +1; 
+                
+                min = Math.min(y,min) ;
             }
-       }
-       return distance[a.length-1];
+        }
+        dp[k] = min ; 
+        return min ; 
     }
     public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
         int a[][] = new int[n][n] ; 
         for(int i = 0 ; i<n-1 ; i++){
             a[i][i+1] = 1; 
         }   
+        dp = new int[n]; 
+        Arrays.fill(dp,-1);
         int ans[] = new int[queries.length] ; 
         for(int i = 0;i<queries.length;i++){
             a[queries[i][0]][queries[i][1]] =1 ; 
             ans[i] = bfs(a,0) ;
+            Arrays.fill(dp,-1);
         }   
         return ans ;
     }
