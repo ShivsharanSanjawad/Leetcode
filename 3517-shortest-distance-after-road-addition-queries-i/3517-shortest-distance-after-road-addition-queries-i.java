e1bd4fceb09c6eb42c93d331a1291ce90fb5d41d
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-11-28 01:14:02 +0530

    Time: 103 ms (71.43%), Space: 45.9 MB (8.45%) - LeetHub

--- 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.java
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.java
@@ -1,33 +1,33 @@
 class Solution {
     int [] dp  ;
-    public int bfs(int a[][],int k){ 
-        if(k==a.length-1){
+    public int bfs(ArrayList<ArrayList<Integer>>a ,int k){ 
+        if(k==a.size()-1){
             return 0 ; 
         }
         if(dp[k]!=-1){
             return dp[k] ; 
         }
         int min = Integer.MAX_VALUE ;
-        for(int i = 0 ; i<a.length ;i++){
-            if(a[k][i]==1){
-                int y  = bfs(a,i) +1; 
-                
+        for(int i = 0 ; i<a.get(k).size() ;i++){
+                int y  = bfs(a,a.get(k).get(i)) +1; 
                 min = Math.min(y,min) ;
-            }
         }
         dp[k] = min ; 
         return min ; 
     }
     public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
-        int a[][] = new int[n][n] ; 
-        for(int i = 0 ; i<n-1 ; i++){
-            a[i][i+1] = 1; 
-        }   
+       ArrayList<ArrayList<Integer>> a = new ArrayList<>() ; 
+       for(int i = 0 ; i<n ; i++){
+            a.add(new ArrayList<>()) ;
+       }
+       for(int i  = 0 ;i<n;i++){
+        a.get(i).add(i+1) ;
+       }
         dp = new int[n]; 
         Arrays.fill(dp,-1);
         int ans[] = new int[queries.length] ; 
         for(int i = 0;i<queries.length;i++){
-            a[queries[i][0]][queries[i][1]] =1 ; 
+           a.get(queries[i][0]).add(queries[i][1]);
             ans[i] = bfs(a,0) ;
             Arrays.fill(dp,-1);
         }   
