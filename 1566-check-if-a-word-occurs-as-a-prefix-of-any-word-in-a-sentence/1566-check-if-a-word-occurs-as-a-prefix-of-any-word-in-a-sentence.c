b4c71dfc2b06d0a25df0fe55f291206fe63fa92a
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-12-02 10:34:15 +0530

    Time: 0 ms (100%), Space: 7.9 MB (42.11%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.c
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.c
@@ -1,25 +1,19 @@
 int isPrefixOfWord(char* s, char* k) {
-    int count = 1 ; 
+    int count = 0 ; 
     int x = 0 ; 
-    while(k[x]!=0&&s[x]==k[x]){
-            x++ ;
-        }
-        if(k[x]==0)
-            return count ;
-    
-    for(int i = 0 ;s[i]!=0 ; ){
-        if(s[i]==' '){
-            count ++ ; 
-            i++ ;
+    char prev = ' ';
+    for(int i = 0 ;s[i]!=0 ; i++){
+        if(prev==' '){
+            count ++ ;
             int x = 0 ; 
-                while(k[x]!=0&&s[x+i]==k[x]){
+            while(k[x]!=0&&s[x+i]==k[x]){
                     x++ ;
-                }
-                    if(k[x]==0){
-                      return count ;
-                    }
-                }
-        else i++;
+            }
+            if(k[x]==0){
+               return count ;
+            }   
+        }
+        prev = s[i] ;
     }
     return -1 ;
 }
