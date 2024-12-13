7e73eb8a44ca280c5ccd048a62e2eccda6b4c646
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-12-14 02:15:57 +0530

    Time: 181 ms (43.19%), Space: 57.1 MB (89.33%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
@@ -12,19 +12,23 @@ class Solution {
         for(int i= 0 ; i<nums.length;i++){
             q.add(new int[]{nums[i],i});
         }
+        int count = 0 ; 
 
-        while(!q.isEmpty()){
+        while(count!=2*nums.length&&!q.isEmpty()){
            int a[] =  q.poll() ;
         //    System.out.println(a[0]+" "+a[1]);
         //    System.out.println(visited[a[1]]);
                 if(!visited[a[1]]){
                 score +=a[0] ;
+                count++ ;
                 int x  = a[1] ; 
                 if(x>0){
                     visited[x-1] = true ;
+                    count++ ;
                 }
                 if(x<nums.length-1){
                     visited[x+1] = true;
+                    count++ ;
                 }
                 visited[a[1]] = true ;
             }
