afa89b1db376c66d24c57aa78a0da85c25b28055
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-12-25 17:16:44 +0530

    Time: 1 ms (98.34%), Space: 40.7 MB (98.34%) - LeetHub

--- 3239-minimum-number-of-operations-to-make-x-and-y-equal/3239-minimum-number-of-operations-to-make-x-and-y-equal.java
+++ 3239-minimum-number-of-operations-to-make-x-and-y-equal/3239-minimum-number-of-operations-to-make-x-and-y-equal.java
@@ -1,25 +1,25 @@
 class Solution {
-    HashMap<Integer,Integer> h ;
+    HashMap<Integer,Integer> dp ;
     public int minimumOperationsToMakeEqual(int x, int y) {
         if(y>x){
             return y-x;
         }
-        h = new HashMap<>() ; 
+        dp= new HashMap<>() ; 
         return get(x,y);
     }
     public int get(int x,int y){
         if(y>=x){
             return y-x;
         }
-        if(h.containsKey(x)){
-            return h.get(x);
+        if(dp.containsKey(x)){
+            return dp.get(x);
         }
         int ans= x-y ;
         ans = Math.min(ans , 1+x%5+get(x/5 , y)) ;
         ans = Math.min(ans , 6-x%5+get(x/5+1,y)) ; 
         ans = Math.min(ans ,1+x%11+get(x/11,y)) ;
         ans = Math.min(ans,12-x%11+get(x/11 +1 ,y)) ;
-        h.put(x,ans) ;
+        dp.put(x,ans) ;
         return ans ;
     }
 }
