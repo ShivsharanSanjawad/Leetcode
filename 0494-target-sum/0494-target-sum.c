9ec17223eda81654d54cc2f64d944beee78b652a
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-12-26 12:08:38 +0530

    Time: 29 ms (36.36%), Space: 40.9 MB (6.06%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.c
@@ -0,0 +1,24 @@
+int **dp ; 
+int get(int *nums , int n , int target , int i,int sum){ 
+        if(i==n){
+            return sum==target ;
+        }
+        if(dp[i][sum+1000]!=-1){
+            return dp[i][sum+1000];
+        }
+        int x =  get(nums,n,target,i+1,sum+nums[i])+get(nums,n,target,i+1,sum-nums[i]) ;
+        dp[i][sum+1000] =x ; 
+        return x;
+}
+int findTargetSumWays(int* nums, int numsSize, int target) {
+        dp = malloc(sizeof(int*)*numsSize);
+        for(int i = 0 ; i<numsSize ;i++){
+            dp[i] = malloc(sizeof(int)*3000);
+        } 
+        for(int i = 0 ; i<numsSize;i++){
+            for(int j = 0 ;j<3000;j++){
+                dp[i][j] = -1 ;
+            }
+        }
+        return get(nums,numsSize ,target,0,0) ;
+}
