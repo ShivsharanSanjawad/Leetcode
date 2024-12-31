5a3ce63690e76098fddc2ca2fd303b804edd8dc7
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2024-12-31 22:42:14 +0530

    Time: 0 ms (100%), Space: 41.2 MB (92.27%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
@@ -1,31 +1,29 @@
 class Solution {
-    int [] dp ;
+    int [] dp ; 
     public int mincostTickets(int[] days, int[] costs) {
-        dp = new int[days.length] ; 
-        Arrays.fill(dp,-1);
+        dp =  new int[days.length] ; 
+        Arrays.fill(dp,-1) ;
         return get(days,0,costs);
     }
     public int get(int []days ,int i,int [] costs){
         if(i>=days.length){
-            return 0;
+            return 0 ; 
         }
         if(dp[i]!=-1){
-            return dp[i] ; 
+            return dp[i] ;
         }
-        int one = costs[0]+  get(days,i+1,costs) ; 
+        int one = costs[0]+get(days,i+1,costs) ; 
         int j = i ; 
         while(j<days.length&&days[j]<days[i]+7){
             j++ ;
         }
-        int two = costs[1]+ get(days,j,costs) ; 
+        int two = costs[1]+get(days,j,costs) ;
         j = i ; 
         while(j<days.length&&days[j]<days[i]+30){
             j++ ;
         }
-        int three = costs[2] + get(days,j,costs) ;
-        int ans = Math.min(one,Math.min(two,three)) ; 
-        dp[i] = ans ; 
-        return ans;
+        int three = costs[2]+get(days,j,costs) ;
+        return dp[i] =  Math.min(one,Math.min(two,three));
     }
     
     
