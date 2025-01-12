2b05edd9a045e372001ec37d4c81638b14fd18de
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-01-12 15:07:34 +0530

    Time: 15 ms (52.38%), Space: 45.6 MB (7.58%) - LeetHub

---
+++ 0477-total-hamming-distance/0477-total-hamming-distance.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int totalHammingDistance(int[] nums) {
+        int zero[] = new int[32] ; 
+        int one[] = new int[32] ; 
+        for(int i =0 ; i<nums.length;i++){
+            int temp = nums[i] ; 
+            for(int j= 0 ; j<32;j++){
+                int x = temp&1 ;
+                if(x==1){
+                    one[j] +=1;
+                }else zero[j] +=1 ;
+                temp>>=1 ; 
+            }
+        }
+        // System.out.println(Arrays.toString(one));
+        // System.out.println(Arrays.toString(zero));
+        int ans = 0 ; 
+        for(int i = 0 ;i<32;i++){
+            ans +=one[i]*zero[i] ;
+        }
+        return ans;
+    }
+}
