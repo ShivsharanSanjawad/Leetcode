4b311964eb5bd6933587eb15bdd3f7446b374d44
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-01-13 18:21:43 +0530

    Time: 10 ms (14.29%), Space: 45.4 MB (34.29%) - LeetHub

--- 2632-apply-bitwise-operations-to-make-strings-equal/2632-apply-bitwise-operations-to-make-strings-equal.java
+++ 2632-apply-bitwise-operations-to-make-strings-equal/2632-apply-bitwise-operations-to-make-strings-equal.java
@@ -1,15 +1,8 @@
 class Solution {
     public boolean makeStringsEqual(String s, String target) {
         int one = 0 ; 
-        int extone = 0 ;
-        int extzero = 0 ; 
         int zero = 0 ;
         for(int i = 0 ; i<s.length();i++){
-            if(s.charAt(i)!=target.charAt(i)){
-                if(s.charAt(i)=='1'){
-                    extone++ ;
-                }else extzero++ ;
-            }
             if(target.charAt(i)=='0'){
                 zero++ ;
             }
@@ -18,8 +11,8 @@ class Solution {
             }
         }
        // System.out.println(one+" "+extzero+" "+extone);
-        if(one==0&&(extzero!=0||extone!=0)){
-            return false ;
+        if(one==0){
+            return s.equals(target);
         }
         if(one!=0&&zero==target.length()){
             return false ;
