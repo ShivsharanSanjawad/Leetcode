4df77b850ba9ae557ecf5308311c14e6f1f48fcf
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-02-15 22:03:17 +0530

    Time: 202 ms (20.69%), Space: 44.5 MB (54.23%) - LeetHub

---
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
@@ -0,0 +1,33 @@
+class Solution {
+    HashMap<Integer,Integer> x ;
+    public int punishmentNumber(int n) {
+        long sum =0 ; 
+        for(int i = 1 ; i<=n;i++){
+            ArrayList<Integer> x =isKaplekar(Long.toString((long)i*i)) ; 
+            //System.out.println(x);
+            if(x.contains(i)){
+                sum +=i*i ;
+            }  
+        }
+        return (int)sum;
+    }
+    public ArrayList isKaplekar(String s){
+      //  System.out.println(s);
+        if(s.length()==1){
+            ArrayList<Integer> a = new ArrayList<>() ;
+            a.add(Integer.parseInt(s));
+            return a;
+        }
+        ArrayList<Integer> ans = new ArrayList<>() ;
+        ans.add(Integer.parseInt(s));
+        for(int i =1 ;i<s.length();i++){
+            int y = Integer.parseInt(s.substring(0,i)) ; 
+            ArrayList<Integer> x = isKaplekar(s.substring(i,s.length()));
+           // System.out.println(x);
+            for(int j=0 ;j<x.size();j++){
+                ans.add(y+x.get(j));
+            }
+        }
+        return ans ;
+    }
+}
