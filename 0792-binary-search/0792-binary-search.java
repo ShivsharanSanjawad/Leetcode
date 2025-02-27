e826c2aee04104fda719d19befd6e989cd2d09c3
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-02-27 23:41:06 +0530

    Time: 0 ms (100%), Space: 45.9 MB (33.48%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -3,7 +3,7 @@ class Solution {
         int s = 0 ; 
         int e = a.length-1 ; 
         while(s<=e){
-            int mid = s + (e-s)/2 ; 
+            int mid = s +((e-s)>>1) ; 
             if(a[mid]==target){
                 return mid ;
             }
