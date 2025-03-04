72e46d932aeada1dca322707b6faed0aa693c890
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-03-04 08:27:20 +0530

    Time: 0 ms (100%), Space: 40.9 MB (14.24%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,11 +1,28 @@
 class Solution {
+    HashMap<Integer,Integer> h ;
     public boolean checkPowersOfThree(int n) {
-      while(n>0){
-       if(n%3==2){
-            return false;
-       }
-       n = n/3 ;
-      }
-      return true;
+        h = new HashMap<>() ;
+        return get(n);
+    }
+    public boolean get(int n){
+        if(n<2){
+            return true ;
+        }
+        if(n==2){
+            return false ;
+        }
+        int x = 0 ; 
+        int temp = n; 
+        while(temp>0){
+            temp /=3 ; 
+            x++ ;
+        }
+        x-- ; 
+        if(h.containsKey(x)){
+            return false ;
+        }
+        h.put(x,0);
+        //System.out.println(x);
+        return get(n-(int)Math.pow(3,x));
     }
 }
