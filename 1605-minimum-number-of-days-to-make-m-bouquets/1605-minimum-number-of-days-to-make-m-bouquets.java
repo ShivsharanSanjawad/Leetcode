0772cded790d965da5f21768aa8e37ec36ed2f42
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-03-15 18:12:51 +0530

    Time: 30 ms (5.35%), Space: 59.5 MB (6.74%) - LeetHub

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int minDays(int[] b, int m, int k) {
+        if(m*k>b.length){
+            return -1 ;
+        }   
+        int min = Integer.MAX_VALUE ; 
+        int max = -1 ; 
+        for(int i=0;i<b.length;i++){
+            if(b[i]>max){
+                max = b[i] ;
+            }
+            if(b[i]<min){
+                min = b[i] ; 
+            }
+        }
+        int s = min ; 
+        int e = max ; 
+        int ans = -1 ;
+        while(e>=s){
+            int mid = s + (e-s)/2 ; 
+           // System.out.println(mid+" "+get(b,mid,m,k));
+            if(get(b ,mid,m ,k)){
+                ans = mid ;
+                e = mid-1 ;
+            }else s = mid+1 ;
+        }
+        return ans ;
+    }
+    public boolean get(int []b , int n , int m , int k){
+        ArrayList<Integer> a = new ArrayList<>() ; 
+        int prev = 0 ;
+        for(int i=0;i<b.length;i++){
+            if(b[i]<=n){
+                prev ++ ;
+            }else {
+                a.add(prev) ; 
+                prev = 0 ; 
+            }
+        }
+        a.add(prev);
+        int count = 0 ; 
+       // System.out.println(a);
+        for(int i=0;i<a.size();i++){
+            count += a.get(i)/k ; 
+        }
+        return count>=m; 
+    }
+}
