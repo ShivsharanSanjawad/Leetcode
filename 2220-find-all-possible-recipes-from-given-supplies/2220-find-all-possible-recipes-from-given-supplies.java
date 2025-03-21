d1b5178dd36551e6759e41f97e43dfc36c0e9d9c
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-03-21 20:12:24 +0530

    Time: 1587 ms (5.07%), Space: 46.5 MB (79.14%) - LeetHub

---
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> in, String[] supplies) {
+        HashMap<String,Integer> ans = new HashMap<>() ;
+        HashMap<String,Integer> s = new HashMap<>() ; 
+        for(int i=0;i<supplies.length;i++){
+            s.put(supplies[i],0) ;
+        }
+        int z = supplies.length ;
+        while(z-->0){
+        for(int i=0;i<in.size();i++){
+            boolean flag = true ;
+            if(ans.containsKey(in.get(i))){
+                continue ;
+            }
+            for(int j=0;j<in.get(i).size();j++){
+                if(!s.containsKey(in.get(i).get(j))&&!ans.containsKey(in.get(i).get(j))){
+                    flag = false ; 
+                    break ;
+                }
+            }
+            if(flag){
+                ans.put(recipes[i],0) ;
+            }
+        }
+        }
+        ArrayList<String> x = new ArrayList<>() ;
+        for(String k:ans.keySet()){
+            x.add(k) ;
+        }
+        return x; 
+    }
+}
