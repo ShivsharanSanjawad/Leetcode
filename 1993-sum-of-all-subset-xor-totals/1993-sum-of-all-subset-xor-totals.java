9a3d9fd0ea9652a29704ee25b8468ec2dd53c234
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-04-05 17:22:47 +0530

    Time: 0 ms (100%), Space: 41.3 MB (36.98%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
@@ -1,32 +1,13 @@
 class Solution {
-     static int xor = 0 ;
-    public   int subsetXORSum(int[] nums) {
-        xor = 0  ;
-        xOR(new ArrayList<Integer>(),nums , 0) ; 
-        return xor  ;
-    }
-
-    public static void xOR(ArrayList<Integer>a,int []nums , int c){
-        if(c==nums.length){
-            int i = 0 ; 
-            int x =0 ;
-            while(i<a.size()){
-                 x = x^a.get(i) ; 
-                 i++ ;
-            }
-            xor  = xor + x;
-            return  ;
-         
+    public int get(int []nums,int i,int xor){
+        if(i>=nums.length){
+            return xor  ; 
         }
-          xOR(a , nums , c+1) ; 
-            a.add(nums[c]) ;
-            xOR(a,nums , c+1) ;
-            int i = 0 ; 
-            int x =0 ;
-            while(i<a.size()){
-                 x = x^a.get(i) ; 
-                 i++ ;
-            }
-    
+        int choose = get(nums,i+1,xor^nums[i]) ;
+        int nonChoose = get(nums,i+1,xor) ; 
+        return choose+nonChoose ;
+    }
+    public int subsetXORSum(int[] nums) {
+        return get(nums,0,0) ;
     }
 }
