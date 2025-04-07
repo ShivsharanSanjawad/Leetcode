17560c919980bca567acc6c1324842f17d339fa3
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-04-07 10:52:29 +0530

    Time: 249 ms (5.02%), Space: 91.4 MB (5.07%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,31 +1,36 @@
+import java.util.*;
+
 class Solution {
-    public List<Integer> largestDivisibleSubset(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        List<Integer> result = new ArrayList<>();
-        Map<String, List<Integer>> memo = new HashMap<>();
-        return dfs(nums, -1, 0, memo);
-    }
+    HashMap<Integer, List<Integer>> memo;
 
-    private List<Integer> dfs(int[] nums, int prevIdx, int currIdx, Map<String, List<Integer>> memo) {
-        if (currIdx >= nums.length) return new ArrayList<>();
-
-        String key = prevIdx + "," + currIdx;
-        if (memo.containsKey(key)) return memo.get(key);
-
-        // Option 1: Skip current
-        List<Integer> notPick = dfs(nums, prevIdx, currIdx + 1, memo);
-
-        // Option 2: Pick current if divisible
-        List<Integer> pick = new ArrayList<>();
-        if (prevIdx == -1 || nums[currIdx] % nums[prevIdx] == 0) {
-            pick.add(nums[currIdx]);
-            pick.addAll(dfs(nums, currIdx, currIdx + 1, memo));
+    public List<Integer> helper(int[] nums, int prevIndex, int currIndex) {
+        if (currIndex == nums.length) {
+            return new ArrayList<>();
         }
 
-        // Choose longer subset
-        List<Integer> result = (pick.size() > notPick.size()) ? pick : notPick;
-        memo.put(key, result);
-        return result;
+        int key = prevIndex * 1001 + currIndex; // 201 > max input size (to keep key unique)
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        // Option 1: Skip current element
+        List<Integer> without = helper(nums, prevIndex, currIndex + 1);
+
+        // Option 2: Take current element if divisible
+        List<Integer> with = new ArrayList<>();
+        if (prevIndex == -1 || nums[currIndex] % nums[prevIndex] == 0) {
+            with.add(nums[currIndex]);
+            with.addAll(helper(nums, currIndex, currIndex + 1));
+        }
+
+        List<Integer> res = with.size() > without.size() ? with : without;
+        memo.put(key, res);
+        return res;
+    }
+
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        Arrays.sort(nums); // always sort for this problem
+        memo = new HashMap<>();
+        return helper(nums, -1, 0); // start with no element picked
     }
 }
