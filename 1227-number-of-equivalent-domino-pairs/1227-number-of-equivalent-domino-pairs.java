e58552bf11e553efb4ad200f4549e09071db4b84
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-05-04 13:28:59 +0530

    Time: 4 ms (75.57%), Space: 52.8 MB (74.3%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.java
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.java
@@ -1,18 +1,27 @@
 class Solution {
     public int numEquivDominoPairs(int[][] d) {
-        HashMap<Integer,Integer> h = new HashMap<>() ; 
+       // HashMap<Integer,Integer> h = new HashMap<>() ; 
+       // ha yes mai hashmap use kar raha tha , but yes we can use array too simply 
+       int a[] = new int[100] ;
+       Arrays.fill(a,-1);
         for(int i=0;i<d.length;i++){
             int x = d[i][0]*10 + d[i][1] ; 
             int y = d[i][1]*10 + d[i][0] ; 
-            if(h.containsKey(x)){
-                h.put(x,h.get(x)+1);
-            }else {
-                h.put(y,h.getOrDefault(y,0)+1);
+            if(a[x]!=-1){
+                a[x]++ ;
+            }else{
+                if(a[y]==-1){
+                    a[y] = 0 ;
+                }
+                a[y]++ ;
             }
         }
         int count = 0 ; 
-        for(Integer key:h.keySet()){
-            count += h.get(key)*(h.get(key)-1)/2 ; 
+        for(int i=0;i<100;i++){
+            if(a[i]==-1){
+                continue ;
+            }
+            count += a[i]*(a[i]-1)/2 ;
         }
         return count ;
     }
