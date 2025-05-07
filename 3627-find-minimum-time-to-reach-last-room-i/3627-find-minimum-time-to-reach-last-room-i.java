f05f7e8d7a57b19e498dcadbb639929048c49db9
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-05-07 22:15:52 +0530

    Time: 19 ms (12.77%), Space: 45.1 MB (53.27%) - LeetHub

---
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.java
@@ -0,0 +1,39 @@
+class Solution {
+    int directions [][] = {{0,1},{1,0},{-1,0},{0,-1}} ;
+    int m ; 
+    int n ;
+    public boolean valid(int x ,int y){
+        return x>=0&&y>=0&&x<m&&y<n ;
+    }
+    public int minTimeToReach(int[][] moveTime) {
+        PriorityQueue<int[]> q = new PriorityQueue<>((x,y)->Integer.compare(x[2],y[2])) ; 
+        int [] a = new int[3] ; 
+        m = moveTime.length ;
+        n = moveTime[0].length ;
+        boolean [][] visited = new boolean[n][n] ; 
+        a[0] = 0 ; 
+        a[1] = 0 ;
+        a[2] = 0 ; 
+        int cost = 0 ; 
+        q.add(a);
+        while(!q.isEmpty()){
+            int b[] = q.poll() ;
+            if(visited[b[0]][b[1]]){
+                continue ;
+            }
+            visited[b[0]][b[1]] = true ;
+            if(b[0]==m-1&&b[1]==n-1){
+                return b[2] ;
+            }
+            for(int d[]:directions){
+                int x = b[0] + d[0] ; 
+                int y = b[1] + d[1] ; 
+                if(valid(x,y)){
+                   int dist = Math.max(b[2],moveTime[x][y])+1;
+                   q.add(new int[]{x,y,dist}) ;  
+                }
+            }
+        }
+        return -1 ;
+    }
+}
