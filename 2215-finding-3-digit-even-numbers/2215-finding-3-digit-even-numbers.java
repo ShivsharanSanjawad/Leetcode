efc23f0c373bc4bb36aaf14dfcc938091deb34ea
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-05-12 11:23:03 +0530

    Time: 3 ms (97.44%), Space: 44.6 MB (90.36%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.java
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.java
@@ -1,28 +1,31 @@
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
-        boolean flags[] = new boolean[1001];
-        int n = digits.length ;
-        for(int i=0;i<n;i++){
-            if(digits[i]==0) continue ;
-            for(int j=0;j<n;j++){
-                if(i==j) continue;
-                for(int k=0;k<n;k++){
-                    if(j==k||i==k) continue;
-                    if(digits[k]%2!=0) continue ;
-                    int z = digits[i]*100+digits[j]*10+digits[k];
-                    flags[z] = true ; 
+        int [] freq = new int[10] ;
+        for(int i=0;i<digits.length;i++){
+            freq[digits[i]]++ ;
+        }
+        ArrayList<Integer> y = new ArrayList<>(); 
+        for(int i=100;i<1000;i+=2){
+            int [] freq2 = new int[10] ; 
+            int x = i ; 
+            while(x>0){
+                freq2[x%10]++ ;
+                x /=10 ;
+            }
+            boolean flag = true ;
+            for(int j=0;j<10;j++){
+                if(freq2[j]>freq[j]){
+                    flag = false ; 
+                    break ;
                 }
             }
-        }
-        ArrayList<Integer> x = new ArrayList<>() ; 
-        for(int i=0;i<1001;i++){
-            if(flags[i]){
-                x.add(i);
+            if(flag){
+                y.add(i);
             }
         }
-        int a[] = new int[x.size()] ;
-        for(int i=0;i<x.size();i++){
-            a[i] = x.get(i) ;
+        int a[] = new int[y.size()] ;
+        for(int i=0;i<y.size();i++){
+            a[i] = y.get(i) ;
         }
         return a;
     }
