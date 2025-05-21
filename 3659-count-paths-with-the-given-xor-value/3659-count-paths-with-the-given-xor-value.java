8a4a594b027e3d1974b5be0a083dc134858498ce
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-05-22 02:04:22 +0530

    Time: 89 ms (41.33%), Space: 59.7 MB (28%) - LeetHub

--- 3659-count-paths-with-the-given-xor-value/3659-count-paths-with-the-given-xor-value.java
+++ 3659-count-paths-with-the-given-xor-value/3659-count-paths-with-the-given-xor-value.java
@@ -2,27 +2,31 @@ class Solution {
     long mod = 1000000007;
     long[][][]dp;
     public int countPathsWithXorValue(int[][] grid, int k) {
-        int m = grid.length ;
-        int n = grid[0].length ;
-        dp = new long[m][n][16] ;
-        for(int i=0;i<16;i++){
-            dp[m-1][n-1][i] = (i==k)?1:0 ; 
-        }
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                if(i==m-1&&j==n-1) continue ;
-                for(int xor=0;xor<16;xor++){
-                    long ways = 0 ; 
-                    if(i+1<m){
-                        ways = (ways+ dp[i+1][j][xor^grid[i+1][j]])%mod ;
-                    }
-                    if(j+1<n){
-                        ways = (ways + dp[i][j+1][xor^grid[i][j+1]])%mod ;
-                    }
-                    dp[i][j][xor] = ways ;
-                }
+        dp = new long[grid.length+1][grid[0].length+1][17] ;
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                Arrays.fill(dp[i][j],-1);
             }
         }
-        return (int)dp[0][0][grid[0][0]];
+        return (int)get(grid,k,0,0,grid[0][0]) ;
+    }
+    public long get(int [][]grid,int k,int i,int j,int xor){
+        int m = grid.length ;
+        int n = grid[0].length ;
+        if(dp[i][j][xor]!=-1){
+            return dp[i][j][xor];
+        }
+        if(i==(m-1)&&j==(n-1)){
+            return xor==k?1:0 ; 
+        }
+        long ways = 0 ; 
+        if(i+1<m){
+            ways = (ways+ get(grid,k,i+1,j,xor^grid[i+1][j]))%mod ;
+        }
+        if(j+1<n){
+            ways = (ways + get(grid,k,i,j+1,xor^grid[i][j+1]))%mod ;
+        }
+        dp[i][j][xor] = ways ;
+        return ways ;
     }
 }
