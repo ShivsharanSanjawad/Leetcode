006c935cfabdf7c2a12b66c71cf4542aa01b1672
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-05-22 01:51:41 +0530

    Time: 92 ms (37.33%), Space: 60.1 MB (28%) - LeetHub

--- 3659-count-paths-with-the-given-xor-value/3659-count-paths-with-the-given-xor-value.java
+++ 3659-count-paths-with-the-given-xor-value/3659-count-paths-with-the-given-xor-value.java
@@ -1,16 +1,20 @@
 class Solution {
     long mod = 1000000007;
-    HashMap<Long,Long> dp ;
+    long[][][]dp;
     public int countPathsWithXorValue(int[][] grid, int k) {
-        dp = new HashMap<>();
+        dp = new long[grid.length+1][grid[0].length+1][17] ;
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                Arrays.fill(dp[i][j],-1);
+            }
+        }
         return (int)get(grid,k,0,0,grid[0][0]) ;
     }
     public long get(int [][]grid,int k,int i,int j,int xor){
         int m = grid.length ;
         int n = grid[0].length ;
-        long x = ((long)i<<13) | ((long)j<<4) | (xor);
-        if(dp.containsKey(x)){
-            return dp.get(x) ;
+        if(dp[i][j][xor]!=-1){
+            return dp[i][j][xor];
         }
         if(i==(m-1)&&j==(n-1)){
             return xor==k?1:0 ; 
@@ -22,7 +26,7 @@ class Solution {
         if(j+1<n){
             ways = (ways + get(grid,k,i,j+1,xor^grid[i][j+1]))%mod ;
         }
-        dp.put(x,ways) ;
+        dp[i][j][xor] = ways ;
         return ways ;
     }
 }
