8c238580e83c70162397c16948f846dfc6ab209b
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-05-25 22:58:50 +0530

    Time: 20 ms (75.84%), Space: 50.8 MB (76.24%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+       // return get(text1,text1.length()-1,text2,text2.length()-1);
+       int [][] dp = new int[text1.length()+1][text2.length()+1] ;
+       for(int i=0;i<dp.length;i++){
+            dp[i][0] = 0 ;  
+       }
+       for(int i=0;i<dp[0].length;i++){
+            dp[0][i] = 0 ;
+       }
+       for(int i=0;i<text1.length();i++){
+            for(int j=0;j<text2.length();j++){
+                if(text1.charAt(i)==text2.charAt(j)){
+                    dp[i+1][j+1] = dp[i][j]+1 ;
+                }else {
+                    dp[i+1][j+1] = (int)Math.max(dp[i][j+1],dp[i+1][j]);
+                }
+            }
+       }
+       return dp[text1.length()][text2.length()];
+    }
+
+    public int get(String x,int i,String y,int j){
+        if(i==-1||j==-1){
+            return 0 ;
+        }
+        if(x.charAt(i)==y.charAt(j)){
+            return 1+get(x,i-1,y,j-1);
+        }
+        return Math.max(get(x,i-1,y,j),get(x,i,y,j-1));
+    }
+}
