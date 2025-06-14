a0b611f420fb6ec2542775f8459628a194460f8b
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-06-14 14:06:42 +0530

    Time: 21 ms (7.8%), Space: 56.3 MB (48.78%) - LeetHub

---
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minimizeMax(int[] nums, int p) {
+        Arrays.sort(nums);
+        int s = 0 ; 
+        int e = nums[nums.length-1] - nums[0]  ; 
+        int ans = e ;
+        while(e>s){
+            int mid = s + (e-s)/2 ; 
+            if(get(nums,p,mid)){
+                ans = mid ; 
+                e = mid  ;
+            }else s = mid +1 ;
+        }
+        return ans ;
+    }
+    public boolean get(int a[] ,int p , int mid){
+        int count = 0 ; 
+        boolean prev = false ;
+        for(int i=0;i<a.length-1;i++){
+            if(!prev&&a[i+1]-a[i]<=mid){
+                count++ ;
+                prev = true ;
+            }else prev = false ;
+        }
+        return count>=p ;
+    }
+}
