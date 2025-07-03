a8c92299d051ec195fe8dd111df2e0a757f3c26b
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-07-03 10:10:16 +0530

    Time: 2 ms (79.8%), Space: 42.8 MB (49.02%) - LeetHub

---
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public char kthCharacter(int k) {
+        StringBuilder s = new StringBuilder();
+        s.append("a");
+        int x = get(k) ;
+        while(x-->0){
+            s.append(get2(s.toString()));
+        }
+       // System.out.println(s);
+        return s.toString().charAt(k-1);
+    }
+    public int get(int k){
+        if(k<2){
+            return 1;
+        }
+        return 1+get(k>>1);
+    }
+    public String get2(String s){
+        char[] c = new char[s.length()];
+        for(int i=0;i<s.length();i++){
+            int x = (s.charAt(i) -'a' + 1)%26+ 97 ;
+            c[i] = (char)x;
+        }
+        return new String(c);
+    }
+}
