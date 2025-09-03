4ae86dd65d02fa63561fefabfdbdd467a899b566
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2025-09-04 01:18:14 +0530

    Time: 5 ms (95.1%), Space: 44.5 MB (57.84%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.java
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.java
@@ -8,23 +8,11 @@ class Solution {
         });
         int count = 0  ;
         for(int i=0;i<points.length;i++){
+            int maxY = Integer.MIN_VALUE ; 
             for(int j=i+1;j<points.length;j++){
-                if(i==j){
-                    continue ; 
-                }
-                if(points[i][0]!=points[j][0]&&points[i][1]<points[j][1]){
-                    continue ;
-                }
-                boolean flag = true ; 
-                for(int k=0;k<points.length;k++){
-                    if(k==i||k==j) continue ;
-                    if((points[k][0]>=points[i][0]&&points[k][0]<=points[j][0])&&(points[k][1]<=points[i][1]&&points[k][1]>=points[j][1])){
-                        flag = false ; 
-                        break ;
-                    }
-                }
-                if(flag){
-                    count++ ;
+                if(points[j][1]>maxY&&points[j][1]<=points[i][1]){
+                    count++ ; 
+                    maxY = points[j][1] ;
                 }
             }
         }
